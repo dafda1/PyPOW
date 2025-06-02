6c64bf78c68920ba7c56114ccac0f3ea0fdf9163
dafda1 <dafda1@st-andrews.ac.uk>
2025-06-02 17:04:16 +0100

    Create RunConversion.bat

---
+++ RunConversion.bat
@@ -0,0 +1,3 @@
+call C:\Users\dafda1\Anaconda3\envs\scripting\Lib\venv\scripts\common\activate
+C:\Users\dafda1\Anaconda3\envs\scripting\python.exe C:\Users\dafda1\MyPackages\PyPOW\run_XY_conversion.py %*
+pause
