137b6cf00ee2655b86dff42c8361096c2b9e59d1
dafda1 <dafda1@st-andrews.ac.uk>
2025-07-01 15:44:05 +0100

    Update RunConversion.bat

--- RunConversion.bat
+++ RunConversion.bat
@@ -1,3 +1,3 @@
 call C:\Users\dafda1\Anaconda3\envs\scripting\Lib\venv\scripts\common\activate
-C:\Users\dafda1\Anaconda3\envs\scripting\python.exe C:\Users\dafda1\MyPackages\PyPOW\run_XY_conversion.py %*
-pause
+@C:\Users\dafda1\Anaconda3\envs\scripting\python.exe C:\Users\dafda1\MyPackages\PyPOW\run_XY_conversion.py %*
+@pause
