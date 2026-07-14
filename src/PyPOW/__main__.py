0132c9c0b284a8be2df3b055297f1b58dbb949b2
Max Pelly <mp250@st-andrews.ac.uk>
2026-07-14 20:29:47 +0000

    Create PyPOW scrupt in path on build

--- src/PyPOW/__main__.py
+++ src/PyPOW/__main__.py
@@ -13,13 +13,6 @@ from PyPOW.import_module import extract_intended_wavelength as get_wvl
 
 from sys import argv
 
-#%% check input and get list of files
-
-if len(argv) < 2:
-    raise ValueError("Must specify filenames to convert.")
-else:
-    files_to_convert = argv[1:]
-
 #%% define conversion functions
 
 def change_file_extension (filename):
@@ -28,25 +21,33 @@ def change_file_extension (filename):
 def convert_file (filename):
     df, meta = import_xrdml_data(filename, include_metadata = True)
     newfile = change_file_extension(filename)
-    
+
     #initialise data array and write 2 theta and intensity columns
     data = np.empty((df.index.size, 2), dtype = float)
     data[:, 0] = df["2Theta (deg)"]
     data[:, 1] = df["Intensity (counts)"]
-    
+
     #save as text (xy) file
     np.savetxt(newfile, data, fmt = ("%.3f", "%.1f"),
                header = "wavelength = %.5f %s\n" % get_wvl(meta['usedWavelength']) +\
                         "2theta (deg), Intensity (counts)")
-    
+
     #save dataframe as csv
     df.to_csv(filename[:-5] + "csv")
-    
     return newfile
 
 #%% run conversions
+def convert_files():
 
-for filename in files_to_convert:
-    print(f">> Converting file {filename}.")
-    newfile = convert_file(filename)
-    print(f" >> Done. Created file {newfile}.")
+    if len(argv) < 2:
+        raise ValueError("Must specify filenames to convert.")
+    else:
+        files_to_convert = argv[1:]
+
+    for filename in files_to_convert:
+        print(f">> Converting file {filename}.")
+        newfile = convert_file(filename)
+        print(f" >> Done. Created file {newfile}.")
+
+if __name__ == "__main__":
+    convert_files()
